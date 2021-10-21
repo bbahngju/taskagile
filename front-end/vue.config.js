1c579982b0a69396825a9cb10942b472b1a150c3
Juyoung Bahng <qkdwn0313@gmail.com>
2021-10-22 03:04:04 +0900

    combine build process & bridge the communication

---
+++ front-end/vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+  outputDir: path.resolve(__dirname, "../src/main/resources/static"),
+  devServer: {
+    port: 3030,
+    proxy: {
+      '/api*': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+}
